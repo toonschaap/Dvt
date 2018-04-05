c213ec9615ff1ecf92b89322946f92c98928a042
Toon <30626397+toonschaap@users.noreply.github.com>
2018-04-05 10:52:57 +0200

    Add files via upload

---
+++ Health/Assets/Movescript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movescript : MonoBehaviour {
+
+	float dirX, dirY;
+	public float moveSpeed = 5f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dirX = Input.GetAxis ("Horizontal");
+		dirY = Input.GetAxis ("Vertical");
+	}
+	void FixedUpdate ()
+	{
+		rb.velocity = new Vector2 (dirX * moveSpeed, dirY * moveSpeed);
+	}
+}
